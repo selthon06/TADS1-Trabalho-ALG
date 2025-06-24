dfe62ac1a190fceef640c7f4cad45545f63b22c7
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-24 14:48:27 -0400

    Atualizar o dados_eventos.py
    adicionando os participantes nos eventos

--- dados_eventos.py
+++ dados_eventos.py
@@ -3,18 +3,18 @@ eventos = [
         "nome": "Fazendo a diferença no IF",
         "data": "2025-06-15",
         "tema": "Como lidar com as atividades do IF",
-        "participantes": []
+        "participantes": [1, 2]
     },
     {
         "nome": "Desenvolver na Web",
         "data": "2025-06-20",
         "tema": "aprendendo a desenvolver e programar sites",
-        "participantes": []
+        "participantes": [3]
     },
     {
         "nome": "Palestra sobre Segurança",
         "data": "2025-06-25",
         "tema": "Segurança",
-        "participantes": []
+        "participantes": [2, 3, 4]
     }
 ]
