9104c2756c92dc85b87239a4b008d7cbd68b3a25
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-24 19:31:36 -0400

    Atualizar o eventos.py
    adicionando funcionalidade de cadastro de eventos com append()

--- eventos.py
+++ eventos.py
@@ -6,3 +6,20 @@ def listar_eventos():
 
     for idx, evento in enumerate(eventos, start=1):
         print(f"{idx}. Nome: {evento['nome']} | Data: {evento['data']} | Tema: {evento['tema']} | Participantes: {len(evento['participantes'])}")
+
+
+def cadastrar_evento():
+    print('\n--- Cadastro de Novo Evento ---')
+    nome = input('Nome do evento: ')
+    data = input('Data (AAAA-MM-DD): ')
+    tema = input('Tema: ')
+
+    novo_evento = {
+        "nome": nome,
+        "data": data,
+        "tema": tema,
+        "participantes": []
+    }
+
+    eventos.append(novo_evento)
+    print(f"Evento '{nome}' cadastrado com sucesso!")
