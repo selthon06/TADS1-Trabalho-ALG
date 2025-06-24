628b3208d1827a3b066bd5887abf8bedbbe1e5a6
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-24 14:44:04 -0400

    Criar dados_participantes.py
    criando alguns dados dos participantes para o trabalho

---
+++ dados_participantes.py
@@ -0,0 +1,26 @@
+participantes = [
+    {
+        "codigo": 1,
+        "nome": "Selthon Leal",
+        "email": "selthon.leal@email.com",
+        "preferencias": ["IF", "Web"]
+    },
+    {
+        "codigo": 2,
+        "nome": "Ana clara",
+        "email": "Ana.clara@email.com",
+        "preferencias": ["IF", "Segurança"]
+    },
+    {
+        "codigo": 3,
+        "nome": "Caio Victor",
+        "email": "caio.victor@email.com",
+        "preferencias": ["Web"]
+    },
+    {
+        "codigo": 4,
+        "nome": "Leonardo Lemos",
+        "email": "leonardo.lemos@email.com",
+        "preferencias": ["Segurança", "IF"]
+    }
+]
