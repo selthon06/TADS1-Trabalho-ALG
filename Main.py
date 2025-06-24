58fc6531fcada05dfb85105d488ef73867669585
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-24 14:11:08 -0400

    Atualizar o Main.py
    criando uma estrutura do meu projeto e menu bem basico

--- Main.py
+++ Main.py
@@ -1,1 +1,21 @@
-print("Olá Mundo!")
+def menu():
+    while True:
+        print("\n=== Sistema de Gerenciamento de Eventos - Comunidade Tech ===")
+        print("1. Listar eventos")
+        print("2. Listar partpantes de um evento")
+        print("3. Buacar participante por código")
+        print("0. Sair")
+
+        opcao = input("Escolhauma opção: ")
+
+        if opcao == '1':
+            print("Função listar eventos ainda não implementada.")
+        elif opcao == '2':
+            print("Função listar participantes ainda não implemetada.")
+        elif opcao == '3':
+            print("Função buscar participante ainda não implementada.")
+        elif opcao == '0':
+            print("Encerrando o sistema. Até mais!")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
