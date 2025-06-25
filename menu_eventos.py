4304649b90f66158182e9a2cb551f378312283fd
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-24 20:25:03 -0400

    Criar menu_eventos.py
    adicionando menu_eventos.py com opções de listar, cadastrar, remover, buscar e adicionar participante"

---
+++ menu_eventos.py
@@ -0,0 +1,30 @@
+import eventos
+
+
+
+def menu_eventos():
+    while True:
+        print('\n========= MENU DE EVENTOS =========')
+        print('(1) - Listar eventos')
+        print('(2) - Cadastrar novo evento')
+        print('(3) - Remover evento')
+        print('(4) - Buscar evento por nome')
+        print('(5) - Adicionar participante a evento')
+        print('(6) - Listar participantes de um evento')
+        print('(0) - Voltar')
+        op = ler_opcao(6)
+
+        if op == 1:
+            eventos.listar_eventos()
+        elif op == 2:
+            eventos.cadastrar_evento()
+        elif op == 3:
+            eventos.remover_evento()
+        elif op == 4:
+            eventos.buscar_evento_por_nome()
+        elif op == 5:
+            eventos.adicionar_participante_evento()
+        elif op == 6:
+            eventos.listar_participantes_evento()
+        elif op == 0:
+            break
