e14d98c26e293edac4a8c4a9373ee8198f66e5c4
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-26 13:05:25 -0400

    Atualizar o menu_eventos.py
    remover participantes duplicados

--- menu_eventos.py
+++ menu_eventos.py
@@ -12,11 +12,12 @@ def menu_eventos():
         print('(7) - Relatório: Temas mais frequentes')
         print('(8) - Relatório: Participantes mais ativos')
         print('(9) - Eventos com menos de 2 participantes')
+        print('(10) - Remover participantes duplicados por evento')
         print('(0) - Voltar')
 
         try:
             op = int(input("Digite uma opção: "))
-            if op < 0 or op > 9:
+            if op < 0 or op > 10:
                 print("Opção inválida!")
                 continue
         except ValueError:
@@ -41,5 +42,7 @@ def menu_eventos():
             eventos.participantes_mais_ativos()
         elif op == 9:
             eventos.eventos_com_poucos_participantes()
+        elif op == 10:
+            eventos.remover_participantes_duplicados()
         elif op == 0:
             break
