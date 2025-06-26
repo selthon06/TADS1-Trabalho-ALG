3644029bb5a1174d1c1e4a03b3aa2ec1dcc70336
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-26 12:39:06 -0400

    Criar menu_participantes.py
    o menu dos participantes

---
+++ menu_participantes.py
@@ -0,0 +1,30 @@
+import participantes
+
+def menu_participantes():
+    while True:
+        print('\n========= MENU DE PARTICIPANTES =========')
+        print('(1) - Listar participantes')
+        print('(2) - Cadastrar novo participante')
+        print('(3) - Remover participante')
+        print('(4) - Buscar participante por código')
+        print('(0) - Voltar')
+
+        try:
+            op = int(input("Digite uma opção: "))
+            if op < 0 or op > 4:
+                print("Opção inválida!")
+                continue
+        except ValueError:
+            print("Digite um número válido.")
+            continue
+
+        if op == 1:
+            participantes.listar_participantes()
+        elif op == 2:
+            participantes.cadastrar_participante()
+        elif op == 3:
+            participantes.remover_participante()
+        elif op == 4:
+            participantes.buscar_participante()
+        elif op == 0:
+            break
