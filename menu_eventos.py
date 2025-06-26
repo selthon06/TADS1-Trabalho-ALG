1e4f58cf0c70e67f37500656572c7aef4ef346e5
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2025-06-26 13:14:30 -0400

    Atualizar o menu_eventos.py
    corrigindo o menu

--- menu_eventos.py
+++ menu_eventos.py
@@ -14,11 +14,12 @@ def menu_eventos():
         print('(9) - Eventos com menos de 2 participantes')
         print('(10) - Remover participantes duplicados por evento')
         print('(11) - Filtrar eventos por tema')
+        print('(12) - Filtrar eventos por faixa de datas')
         print('(0) - Voltar')
 
         try:
             op = int(input("Digite uma opção: "))
-            if op < 0 or op > 11:
+            if op < 0 or op > 12:
                 print("Opção inválida!")
                 continue
         except ValueError:
@@ -44,9 +45,10 @@ def menu_eventos():
         elif op == 9:
             eventos.eventos_com_poucos_participantes()
         elif op == 10:
-  
-        elif op == 11:
-eventos.filtrar_eventos_por_tema()
             eventos.remover_participantes_duplicados()
+        elif op == 11:
+            eventos.filtrar_eventos_por_tema()
+        elif op == 12:
+            eventos.filtrar_eventos_por_data()
         elif op == 0:
             break
